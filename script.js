e55e486d79e236a1f46f10a56c4d5e97562a5b34
Mirabelle GNANGUENON <mirabellegnanguenon6@gmail.com>
2026-07-16 23:30:57 +0200

    Organisation du portfolio avec la nouvelle arborescence

---
+++ script.js
@@ -0,0 +1,73 @@
+
+
+const sections = document.querySelectorAll("section");
+
+let nav = document.querySelector("nav");
+
+const hauteurNav=nav.offsetHeight;
+
+document.querySelector("main").style.paddingTop=hauteurNav+"px";
+
+
+//effet au scroll sur le nav bar
+window.addEventListener("scroll",function(){
+
+    if(window.scrollY>=hauteurNav){
+        nav.classList.add("effet_scroll");
+    }
+    
+    else{
+        nav.classList.remove("effet_scroll");
+    }
+    
+});
+
+//faire en sorte que les debuts de sections affichées restent visibles lorsqu'on clique vers une section i.e rajouter la hauteur du nav qui cache les debuts de sections
+sections.forEach(section => {
+    section.style.scrollMarginTop=hauteurNav+"px";
+});
+
+
+//effet fondu sur les sections competences et projets
+const observer1 = new IntersectionObserver(function(entries) {// entries c'est le tableau qui regroupe Les sections observée. entry Correspond à chaque élément de ce tableau 
+    entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+            // l'élément est visible à l'écran
+            entry.target.classList.add("visible");// ajoute la classe "visible" à entry.target
+        }
+    });
+});
+
+// dis à l'observer quels éléments surveiller
+observer1.observe(document.querySelector("#about"));
+observer1.observe(document.querySelector("#skills"));
+
+document.querySelectorAll("#projects article").forEach(function(article, index) {
+    // index vaut 0 pour le 1er article, 1 pour le 2e, 2 pour le 3e
+    article.style.transitionDelay = (index * 0.15) + "s";
+    observer1.observe(article);
+});
+
+
+//POUR Rajouter un État actif au titre de section dans le Navbar
+
+
+const liens = document.querySelectorAll("nav a");
+
+const observer2 = new IntersectionObserver((entries) => {  //(entries)=>{} équivaut à function(entries){}
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+
+            liens.forEach(lien => {
+                lien.classList.remove("active");
+            });
+
+            const id = entry.target.id;
+            document.querySelector(`nav a[href="#${id}"]`).classList.add("active");
+        }
+    });
+}, {
+    threshold: 0.5
+});
+
+sections.forEach(section => observer2.observe(section));
